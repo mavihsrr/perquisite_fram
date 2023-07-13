5b656ce934f295203d08a6224a3d2e21eb3c6ecc
khuskher <KHUS.KHER@GMAIL.COM>
2023-07-14 00:04:11 +0530

    removed repeated code

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,66 +2,6 @@ const router = require("express").Router();
 const Book = require("../models/Book");
 const books = require("../config/Book.json");
 
-// router.get("/books", async(req, res) => {
-//     try {
-//         const page = parseInt(req.query.page) - 1 || 0;
-//         const limit = parseInt(req.query.limit) || 3;
-//         const search = req.query.search || "";
-//         let sort = req.query.sort || "rating";
-//         let genre = req.query.genre || "All";
-
-//         const genreOptions = [
-//             "Action", 
-//             "Romance", 
-//             "Fantasy", 
-//             "Drama", 
-//             "Crime", 
-//             "Adventure", 
-//             "Thriller", 
-//             "Sci-fi",
-//             "Music", 
-//             "Family"
-//         ];
-
-//         genre === "All" ? (genre = [...genreOptions]) : (genre = req.query.genre.split(","));
-//         req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
-
-
-//         let sortBy = {};
-//         if(sort[1]) {
-//             sortBy[sort[0]] = sort[1];
-//         } else {
-//             sortBy[sort[0]] = "asc";
-//         }
-
-//         const data = await Book.find({ name: { $regex: search, $options: "i"}})
-//             .where("genre")
-//             .in([...genre])
-//             .sort(sortBy)
-//             .skip(page * limit)
-//             .limit(limit);
-
-//         const total = await Book.countDocuments({
-//             genre: { $in: [...genre] },
-//             title: { $regex: search, $options: "i"},
-//         });
-
-//         const response = {
-//             error: false,
-//             total,
-//             page: page+1,
-//             limit,
-//             genres: genreOptions,
-//             data,
-//         };
-
-//         res.status(200).json(response);
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json({ message: "error" });
-//     }
-// });
-
 router.get("/books", async (req, res) => {
 	try {
 		const page = parseInt(req.query.page) - 1 || 0;
@@ -123,7 +63,7 @@ router.get("/books", async (req, res) => {
 	}
 });
 
-
+// inserting data
 // const bookInsert = async () => {
 //     try {
 //         const docs = await Book.insertMany(books);
